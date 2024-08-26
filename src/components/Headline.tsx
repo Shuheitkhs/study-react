26c74f3d0349ad2945ccbaae35b7b2d9b3f40f68
Stk <shuheitakahashi0822@gmail.com>
2024-08-26 11:58:50 +0900

    ♻️ ヘッドラインのコンポーネント化

---
+++ src/components/Headline.tsx
@@ -0,0 +1,39 @@
+import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.css";
+import Image from "next/image";
+
+const inter = Inter({ subsets: ["latin"] });
+
+type HeadlineProps = {
+    title: string;
+  };
+
+export function Headline( {title }: HeadlineProps  ) {
+  return (
+        <div className={styles.description}>
+          <p>
+            Get started by editing&nbsp;
+            <code className={styles.code}>src/pages/{title}.tsx</code>
+          </p>
+          <div>
+            <a
+              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              By{" "}
+              <Image
+                src="/vercel.svg"
+                alt="Vercel Logo"
+                className={styles.vercelLogo}
+                width={100}
+                height={24}
+                priority
+              />
+            </a>
+          </div>
+          <h1>{title}</h1>
+        </div>
+
+  );
+}
