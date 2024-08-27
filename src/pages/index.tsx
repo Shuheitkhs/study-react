a33a35180158ab917efd2c51f2bef9e3f5500484
Stk <shuheitakahashi0822@gmail.com>
2024-08-27 10:36:21 +0900

    ♻️ 不必要なコードの削除

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,7 @@
 import Head from "next/head";
-import { Inter } from "next/font/google";
 import Main from "@/components/Main";
 import { Header } from "@/components/Header";
 import { useEffect, useState } from "react";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   const [count, setCount] = useState(0);
